ceaaac7462dd24c613efb2d738975d95ff59a5f8
Felipe Tomé or 2lMaKeR <neoswar999@gmail.com>
2022-12-18 22:26:07 -0300

    Launch mk2 update

--- AQUI!! - mk1/nervo.js
+++ AQUI!! - mk1/nervo.js
@@ -4,16 +4,12 @@ cx1.addEventListener('mouseout', sair1)
 cx1.addEventListener('click', click1)
 function entrar1(){
     cx1.style.background = 'black'
-    cx1.style.padding= '15px'
-    cx1.style.margin='auto'
 }
 function click1(){
     window.open('https://www.linkedin.com/in/felipe-tomé-98b787192/', '_blank')
 }
 function sair1(){
     cx1.style.background = 'rgba(0, 0, 0, 0.644)'
-    cx1.style.padding='15px'
-    cx1.style.margin='auto'
 }
 var cx2 =window.document.getElementById('caixa2')
 cx2.addEventListener('mouseenter',entrar2)
@@ -21,16 +17,12 @@ cx2.addEventListener('mouseout',sair2)
 cx2.addEventListener('click', click2)
 function entrar2(){
     cx2.style.background='black'
-    cx2.style.padding='15px'
-    cx2.style.margin='auto'
 }
 function click2(){
     window.open('https://github.com/2lMaKeR','_blank')
 }
 function sair2(){
     cx2.style.background='rgba(0, 0, 0, 0.644)'
-    cx2.style.padding='15px'
-    cx2.style.margin='auto'
 }
 var cx3=window.document.getElementById('caixa3')
 cx3.addEventListener('mouseenter', entrar3)
@@ -38,16 +30,12 @@ cx3.addEventListener('mouseout', sair3)
 cx3.addEventListener('click', click3)
 function entrar3(){
     cx3.style.background='black'
-    cx3.style.padding='15px'
-    cx3.style.margin='auto'
 }
 function click3(){
     window.open('https://www.twitch.tv/2lmaker','_blank')
 }
 function sair3(){
     cx3.style.background='rgba(0, 0, 0, 0.644)'
-    cx3.style.padding='15px'
-    cx3.style.margin='auto'
 }
 var cx4=window.document.getElementById('caixa4')
 cx4.addEventListener('mouseenter', entrar4)
@@ -55,16 +43,12 @@ cx4.addEventListener('mouseout', sair4)
 cx4.addEventListener('click', click4)
 function entrar4(){
     cx4.style.background='black'
-    cx4.style.padding='15px'
-    cx3.style.margin='auto'
 }
 function click4(){
     window.open('https://www.instagram.com/lllmaker/','_blank')
 }
 function sair4(){
     cx4.style.background='rgba(0, 0, 0, 0.644)'
-    cx4.style.padding='15px'
-    cx4.style.margin='auto'
 }
 var cx5=window.document.getElementById('cachacinha')
 cx5.addEventListener('mouseenter', entrar5)
@@ -72,14 +56,10 @@ cx5.addEventListener('mouseout', sair5)
 cx5.addEventListener('click', click5)
 function entrar5(){
     cx5.style.background='black'
-    cx5.style.padding='15px'
-    cx5.style.margin='auto'
 }
 function click5(){
     window.open('https://discord.gg/MJ3AJZEGmH','_blank')
 }
 function sair5(){
     cx5.style.background='rgba(0, 0, 0, 0.644)'
-    cx5.style.padding='15px'
-    cx5.style.margin='auto'
 }
