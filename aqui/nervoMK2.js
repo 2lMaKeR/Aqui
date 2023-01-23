57c4118c839fb715ecf6823dd4c843cbded6ac25
Felipe Tomé or 2lMaKeR <neoswar999@gmail.com>
2023-01-22 21:36:49 -0300

    afinando os codigos

---
+++ aqui/nervoMK2.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    var caixa1 = document.getElementById('caixa1').addEventListener('click', function(){
+        window.open('link', '_blank')
+    })
+    var caixa2 = document.getElementById('caixa2').addEventListener('click', function(){
+        window.open('link', '_blank')
+    })
+    var caixa3 = document.getElementById('caixa3').addEventListener('click', function(){
+        window.open('link', '_blank')
+    })
+    var caixa4 = document.getElementById('caixa4').addEventListener('click', function(){
+        window.open('link', '_blank')
+    })
+    var cachacinha = document.getElementById('cachacinha').addEventListener('click', function(){
+        window.open('link', '_blank')
+    })
+}
